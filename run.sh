cac2485664014fb7632fa42cf18bbc10e09afd3e
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2015-10-23 01:22:31 +0900

    quote variables

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
-sudo curl -sSL -o ${WERCKER_INSTALL_DOCKER_PATH} https://get.docker.io/builds/Linux/x86_64/docker-${WERCKER_INSTALL_DOCKER_VERSION}
-sudo chmod +x ${WERCKER_INSTALL_DOCKER_PATH}
+sudo curl -sSL -o "${WERCKER_INSTALL_DOCKER_PATH}" \
+  "https://get.docker.io/builds/Linux/x86_64/docker-${WERCKER_INSTALL_DOCKER_VERSION}"
+sudo chmod +x "${WERCKER_INSTALL_DOCKER_PATH}"
 
