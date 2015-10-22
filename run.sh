830cc50b877f0d64979045577c929eba2124de75
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2015-10-23 02:08:56 +0900

    show progress message in run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,14 @@
 #! /bin/bash
 
-sudo curl -sSL -o "${WERCKER_INSTALL_DOCKER_PATH}" \
-  "https://get.docker.io/builds/Linux/x86_64/docker-${WERCKER_INSTALL_DOCKER_VERSION}"
-sudo chmod +x "${WERCKER_INSTALL_DOCKER_PATH}"
+url="https://get.docker.io/builds/Linux/x86_64/docker-${WERCKER_INSTALL_DOCKER_VERSION}"
+dest="${WERCKER_INSTALL_DOCKER_PATH}"
+
+info "download docker command from ${url} and install to ${dest}"
+sudo curl -sSL -o "${dest}" "${url}"
+sudo chmod +x "${dest}"
+
+info "${dest} --version"
+${dest} --version
+
+info "install docker command completed."
 
